448ff669af900720a24387b5284dbf1a5693c083
Colin Nielsen <colinrnielsen@gmail.com>
2023-06-23 18:00:06 -0600

    feat: update tests

--- test/commands/preview.test.ts
+++ test/commands/preview.test.ts
@@ -1,4 +1,4 @@
-import { handler as hello } from '../../src/commands/preview';
+import { handler as preview } from '../../src/commands/preview';
 import { runWithAnswers, ENTER } from '../utils';
 import * as utils from '../../src/utils';
 
@@ -7,25 +7,14 @@ jest.mock('inquirer/lib/utils/screen-manager');
 jest.mock('../../src/utils');
 
 const logInfo = jest.spyOn(utils, 'logInfo');
-const renderQuestion = jest.spyOn(require('inquirer/lib/utils/screen-manager').prototype, 'render');
 
 afterEach(() => {
   logInfo.mockReset();
-  renderQuestion.mockReset();
 });
 
-describe('hello', () => {
-  it('says hello with the right name, after asking it if not provided', async () => {
-    await runWithAnswers(() => hello({}), ['gabro', ENTER]);
-    expect(renderQuestion.mock.calls[0][0]).toMatchSnapshot();
-    expect(logInfo).toHaveBeenCalledTimes(2);
-    expect(logInfo.mock.calls[0][0]).toMatchSnapshot();
-    expect(logInfo.mock.calls[1][0]).toMatchSnapshot();
-  });
-
-  it('says hello with the right name, without asking it if provided', async () => {
-    await runWithAnswers(() => hello({ name: 'gabro' }));
-    expect(renderQuestion).not.toHaveBeenCalled();
+describe('preview', () => {
+  it('accepts a preview command with a path', async () => {
+    await runWithAnswers(() => preview({ _: ['preview', 'fake/path.sol'], $0: 'preview' }), []);
     expect(logInfo).toHaveBeenCalledTimes(1);
     expect(logInfo.mock.calls[0][0]).toMatchSnapshot();
   });
