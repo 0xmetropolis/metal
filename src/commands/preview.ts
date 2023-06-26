e5ea3b6260f566dc5ff195eb77bd07675c814e5c
Colin Nielsen <colinrnielsen@gmail.com>
2023-06-26 17:55:23 -0600

    feat: :sparkles: load source code after run

--- src/commands/preview.ts
+++ src/commands/preview.ts
@@ -1,8 +1,7 @@
-import yargs = require('yargs');
-import { logError, logInfo, logWarn } from '../utils';
-import { type Options } from 'yargs';
-import { ExecException, spawn } from 'child_process';
+import { type Arguments, type Options } from 'yargs';
 import { METRO_DEPLOY_URL } from '../constants';
+import { loadSolidityFiles, logInfo, logWarn } from '../utils';
+import { getScriptDependencies, loadFoundryConfig, runForgeScript } from '../utils/foundry';
 
 export const command = 'preview';
 export const description = `Generate preview of transactions from your Forge script`;
@@ -17,14 +16,8 @@ export const builder: { [key: string]: Options } = {
   },
 };
 
-function processForgeError({ message }: ExecException) {
-  if (message.includes('error trying to connect'))
-    return 'Could not connect to the RPC, check your internet connection';
-  return message;
-}
-
-// @dev replaces any forge fork aliases with the METRO_DEPLOY_URL
-function configureForgeInputs() {
+// @dev pulls any args from process.argv and replaces any fork-url aliases with the METRO_DEPLOY_URL
+export const configureForgeScriptInputs = (): string[] => {
   // pull anything after `metro preview <path>` as forge arguments
   const initialArgs = process.argv.slice(3);
   const rpcIndex = initialArgs.findIndex(arg => FORGE_FORK_ALIASES.some(alias => alias === arg));
@@ -41,30 +34,24 @@ function configureForgeInputs() {
     : [...initialArgs, '--rpc-url', METRO_DEPLOY_URL];
 
   return formattedArgs;
-}
-
-// @dev returns the terminal status code of the forge script
-// @throws if the forge script fails
-async function runForgeScript(scriptArgs: string[]) {
-  return await new Promise<number>((resolve, reject) => {
-    const forge_script = spawn(`forge script ${scriptArgs.join(' ')}`, { shell: true });
-
-    // log any errors
-    forge_script.on('error', err => {
-      logError('\n' + processForgeError(err) + '\n');
-      reject();
-    });
-    // log any forge output
-    forge_script.stdout.on('data', logInfo);
-    // on completion, resolve the promise
-    forge_script.on('close', resolve); // TODO: code => (code === 0 ? resolve(code) : reject())
-  });
-}
+};
 
 // @dev entry point for the preview command
-export async function handler({ _: [, path] }: yargs.Arguments) {
-  logInfo(`Running Forge Script at ${path}...`);
+export const handler = async ({ _: [, forgeScriptPath] }: Arguments) => {
+  logInfo(`Loading foundry.toml...`);
+  const foundryConfig = loadFoundryConfig();
 
-  const foundryArguments = configureForgeInputs();
+  logInfo(`Running Forge Script at ${forgeScriptPath}...`);
+
+  const foundryArguments = configureForgeScriptInputs();
   await runForgeScript(foundryArguments);
-}
+
+  logInfo(`Forge deployment script ran successfully!`);
+
+  logInfo(`Retreiving Solidity source code...`);
+  const dependencyList = getScriptDependencies(foundryConfig, forgeScriptPath);
+  const solidityFiles = [forgeScriptPath, ...dependencyList];
+  const sourceCode = loadSolidityFiles(solidityFiles);
+
+  console.log(Object.keys(sourceCode));
+};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,27 @@
-import { emojify } from 'node-emoji';
 import chalk from 'chalk';
+import { emojify } from 'node-emoji';
+import { readFileSync } from 'node:fs';
 
-export const logError = (s: string) => console.log(emojify(chalk.bold.red(s)));
+export const logError = (...s: string[]) => console.log(emojify(chalk.bold.red(s.join('\n'))));
 export const logInfo = (s: string) => console.log(emojify(chalk.bold(s)));
-export const logWarn = (...s: string[]) => console.warn(emojify(chalk.bold.yellow(s.join('\n'))));
+export const logWarn = (...s: string[]) =>
+  console.warn(emojify(chalk.bold.yellow('⚠️ ' + s.join('\n') + ' ⚠️')));
 export const logDetail = (s: string) => console.log(emojify(chalk.dim(s)));
+
+// @dev returns kv pairs of solidity file path and source code
+export const loadSolidityFiles = (pathToSolc: string[]): { [pathToSolc: string]: string } => {
+  const sourceCode = pathToSolc.reduce<{ [contractFilePath: string]: string }>(
+    (sourceCodeAcc, path) => {
+      try {
+        const code = readFileSync(path, { encoding: 'utf-8' });
+        return { ...sourceCodeAcc, [path]: code };
+      } catch (e: any) {
+        logError(`Could not find Solidity source file: ${path}`);
+        process.exit(1);
+      }
+    },
+    {},
+  );
+
+  return sourceCode;
+};
