781d845f630674bd786a07be9aa091cdc9bda41a
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2023-07-26 17:47:02 -0400

    feat: :closed_lock_with_key: mock PK by default (#40)
    * feat: :closed_lock_with_key: mock PK by default
    
    * fix: code review
    
    * refactor: move constants
    
    * ci: remove pk override
    
    * ci: fix whitespace

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,32 @@
 import { Network } from './types';
 
 export const SUPPORTED_CHAINS = Object.values(Network) as Network[];
+export const DEFAULT_PRIVATE_KEY =
+  '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'; // hardhat 0 address
 
 export const PREVIEW_SERVICE_URL = process.env.PREVIEW_SERVICE_URL ?? 'http://localhost:1234';
 export const PREVIEW_WEB_URL = process.env.PREVIEW_WEB_URL ?? 'http://localhost:3000';
 export const doNotCommunicateWithPreviewService = !!process.env.NO_PREVIEW_SERVICE;
+
+export const FORGE_FORK_ALIASES = ['--fork-url', '-f', '--rpc-url'];
+export const FORGE_WALLET_OPTIONS = [
+  '-a',
+  '--froms',
+  '-i',
+  '--interactives',
+  '--private-keys',
+  '--private-key',
+  '--mnemonics',
+  '--mnemonic-passphrases',
+  '--mnemonic-derivation-paths',
+  '--mnemonic-indexes',
+  '--keystore',
+  '--password',
+  '--password-file',
+  '-l',
+  '--ledger',
+  '-t',
+  '--trezor',
+  '--aws',
+];
+export const RPC_OVERRIDE_FLAG = '--UNSAFE-RPC-OVERRIDE';
