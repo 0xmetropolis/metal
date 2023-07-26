f42e62982b4faba1faa99fffc003999d8b4b2d66
Colin Nielsen <colinrnielsen@gmail.com>
2023-07-26 21:51:38 +0100

    feat: 🔔s + 😙s

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,19 @@ import { Network } from './types';
 
 export const SUPPORTED_CHAINS = Object.values(Network) as Network[];
 
-export const PREVIEW_SERVICE_URL = process.env.PREVIEW_SERVICE_URL ?? 'http://localhost:1234';
-export const PREVIEW_WEB_URL = process.env.PREVIEW_WEB_URL ?? 'http://localhost:3000';
+// @dev if the script is being run via the mdev command, we are in dev mode
+export const IS_DEV = process.argv[1].includes('mdev');
+
+export const PREVIEW_SERVICE_URL = process.env.PREVIEW_SERVICE_URL
+  ? process.env.PREVIEW_SERVICE_URL
+  : IS_DEV
+  ? 'http://localhost:1234'
+  : 'https://preview-service-225b51c334ef.herokuapp.com';
+
+export const PREVIEW_WEB_URL = process.env.PREVIEW_WEB_URL
+  ? process.env.PREVIEW_WEB_URL
+  : IS_DEV
+  ? 'http://localhost:3000'
+  : 'https://metropolis.sh';
+
 export const doNotCommunicateWithPreviewService = !!process.env.NO_PREVIEW_SERVICE;
