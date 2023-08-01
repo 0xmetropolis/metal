6c8a3161e780a80581bce29c9f60df3446e0972a
Kevin Weaver <kevinweaver2@gmail.com>
2023-07-31 17:38:29 -0700

    Use git --version instead of git -v

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -3,7 +3,7 @@ import { execSync } from 'node:child_process';
 
 export const isGitInstalled = () => {
   try {
-    execSync('git -v', { stdio: 'ignore' });
+    execSync('git --version', { stdio: 'ignore' });
     return true;
   } catch (e: any) {
     return false;
