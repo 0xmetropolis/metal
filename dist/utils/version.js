58f43d8d0b5c3282cc5244d75d2addb6cd39cbbe
Kevin Weaver <kevinweaver2@gmail.com>
2023-08-08 11:08:21 -0700

    Update package.json path

--- dist/utils/version.js
+++ dist/utils/version.js
@@ -5,8 +5,7 @@ const path = require("node:path");
 const fs = require("node:fs");
 // @dev gets the version of the CLI
 const getCLIVersion = () => {
-    //TODO correct path
-    const packageJSONPath = path.resolve(__dirname, '../package.json');
+    const packageJSONPath = path.resolve(__dirname, '../../package.json');
     const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf8'));
     return packageJSON.version;
 };
--- src/utils/version.ts
+++ src/utils/version.ts
@@ -3,8 +3,7 @@ import * as fs from 'node:fs';
 
 // @dev gets the version of the CLI
 export const getCLIVersion = (): string => {
-  //TODO correct path
-  const packageJSONPath = path.resolve(__dirname, '../package.json');
+  const packageJSONPath = path.resolve(__dirname, '../../package.json');
   const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, 'utf8'));
 
   return packageJSON.version;
