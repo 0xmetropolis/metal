06b140d3c774094c3c4e3da657d0dbbca0e91b6c
Colin Nielsen <colinrnielsen@gmail.com>
2023-08-08 18:07:01 -0600

    build: ignore dist, fix ci

--- dist/commands/preview.d.ts
+++
@@ -1,7 +0,0 @@
-import { Command } from '@oclif/core';
-export default class Preview extends Command {
-    static description: string;
-    static examples: string[];
-    static strict: boolean;
-    run(): Promise<void>;
-}
