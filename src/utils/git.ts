1b3a924bf86095c1bea19f4f9bbeb1d7912280cc
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2023-09-20 14:03:07 -0600

    Update git.ts

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -2,7 +2,6 @@ import { GitMetadata, HexString, RepoMetadata } from '../types';
 import { execSync } from 'node:child_process';
 import { cwd } from 'node:process';
 import { exit, logDebug } from '.';
-import { GitMetadata, HexString, RepoMetadata } from '../types/index';
 
 export const isGitInstalled = () => {
   try {
