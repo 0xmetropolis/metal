b56fb350dafc40180fe360cf730569e8c7a31345
Colin Nielsen <colinrnielsen@gmail.com>
2023-10-13 15:25:19 -0600

    refactor: rename cliPrompt -> cliCommand

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -2,20 +2,22 @@ import { logDebug } from '.';
 import { PREVIEW_SERVICE_URL } from '../constants';
 import { checkAuthentication } from './auth';
 
-export const sendCliCommandAnalytics = async (prompt: 'import' | 'deploy' | 'preview' | 'auth') => {
+export const sendCliCommandAnalytics = async (
+  cliCommand: 'import' | 'deploy' | 'preview' | 'auth',
+) => {
   const auth = await checkAuthentication();
   const authHeaders =
     auth.status === 'authenticated' ? { Authorization: `Bearer ${auth.access_token}` } : {};
 
   try {
-    logDebug(`sending analytics for ${prompt}`);
+    logDebug(`sending analytics for ${cliCommand}`);
     const request = await fetch(`${PREVIEW_SERVICE_URL}/analytics/cli-command`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         ...authHeaders,
       },
-      body: JSON.stringify({ cliPrompt: prompt }),
+      body: JSON.stringify({ cliCommand }),
     });
 
     logDebug(request.status);
