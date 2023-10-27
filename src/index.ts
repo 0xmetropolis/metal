4684a616890c5e1cd750f6c2fd78bfd65fb8186d
Colin Nielsen <33375223+colinnielsen@users.noreply.github.com>
2023-10-27 14:18:27 -0600

    feat: introduce global debug flag (#107)

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,14 @@ import * as yargs from 'yargs';
 
 // prettier-ignore
 yargs
+  // @ts-ignore
+  .middleware(({ debug }) => {
+    // set a debug flag for all commands
+    process.env.YARG_DEBUG = debug ? '1' : '0' 
+  })
   .commandDir('commands')
   .demandCommand(1)
   .help()
   .version()
+  .showHelpOnFail(false)
   .argv;
